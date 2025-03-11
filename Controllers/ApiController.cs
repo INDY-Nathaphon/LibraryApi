58061ef2c712141b39048eb8b77e608387520fe9
indycrm <Nathaphon.na@chococrm.com>
2025-03-11 10:04:09 +0700

    Add controller.

---
+++ Controllers/ApiController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ApiController : ControllerBase
+    {
+        private readonly ILogger<ApiController> _logger;
+
+        public ApiController(ILogger<ApiController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
